b1876af2ee1334c2edbeb612e58c1a640072ae5d
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-09-26 11:06:57 +0300

    Abstract tower intuition added

---
+++ Assets/Scripts/AbstractTower.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractTower : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject m_projectilePrefab;
+
+    [SerializeField]
+    protected float m_shootInterval = 0.5f;
+
+    protected float lastShotTime;
+
+    protected List<Monster> monsterList;
+
+    private AbstractTower()
+    {
+        monsterList = new List<Monster>();
+    }
+
+    private void Start()
+    {
+        gameObject.AddComponent<ObjectPool>();
+    }
+
+    public virtual void UpdateMonsterList() { }
+
+    public virtual void SelectTarget() { }
+
+    public virtual void Shoot(Monster target)
+    {
+        lastShotTime = Time.time;
+    }
+
+    public virtual void Update()
+    {
+        if(m_projectilePrefab == null)
+        {
+            return;
+        }
+
+        if(Time.time - lastShotTime < m_shootInterval)
+        {
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+    private List<GameObject> bulletPool;
+
+    void Start () {
+        var objToSpawn = new GameObject("Object pool");
+        objToSpawn.transform.parent = transform;
+    }
+	
+	
+}
